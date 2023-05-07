2ff9de2ba826c6baadad4bc67fa39c3cf80cd6da
hkrawczyk <hello@hkrawczyk.com>
2023-05-08 01:30:41 +0200

    fixed auth

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,14 +1,12 @@
 const {decode} = require("next-auth/jwt");
 const cookie = require("cookie");
-// const meta = process.env.ENV === "production" ? "__Host-next-auth.csrf-token" : "next-auth.csrf-token"
+const meta = process.env.ENV === "production" ? "__Host-next-auth.session-token" : "next-auth.session-token"
 async function getAccessToken(req) {
     if(!req.headers.cookie) {
         return await checkSelfCall(req.headers)
     }
     const cookies = cookie.parse(req.headers.cookie)
-    console.log("cookie", cookies, process.env.NEXTAUTH_SECRET)
-    const sessionToken = cookies["next-auth.csrf-token"].split("|")[0]
-    console.log("decode", { token: sessionToken, secret: process.env.NEXTAUTH_SECRET })
+    const sessionToken = cookies[meta]
 
     if (!process.env.NEXTAUTH_SECRET) throw new Error()
     return await decode({ token: sessionToken, secret: process.env.NEXTAUTH_SECRET })
