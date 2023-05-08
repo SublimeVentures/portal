9770127f266ff27efe7d431dca5b4af514e766b5
hkrawczyk <hello@hkrawczyk.com>
2023-05-08 02:03:44 +0200

    font

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,90 +5,51 @@ import {fetchSessionData} from "@/fetchers/login.fetcher";
 import PAGE from "@/routes";
 const meta = process.env.ENV === "production" ? "__Host-next-auth.csrf-token" : "next-auth.csrf-token"
 
-const providers = [
-    CredentialsProvider({
-        name: "Ethereum",
-        credentials: {
-            message: {
-                label: "Message",
-                type: "text",
-                placeholder: "0x0",
-            },
-            signature: {
-                label: "Signature",
-                type: "text",
-                placeholder: "0x0",
-            },
-        },
-        async authorize(credentials) {
-            try {
-                const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"))
-                // const nextAuthUrl = new URL(process.env.NEXTAUTH_URL)
-                // console.log("cookies",req.cookies, meta)
-                // const csrf = req.cookies[meta].split("|")[0]
-                // const result = await siwe.verify({
-                //     signature: credentials?.signature || "",
-                //     domain: nextAuthUrl.host,
-                //     nonce: csrf,
-                // })
-
-                if (!result.success) return null;
-                const type = await fetchSessionData(siwe.address)
-
-                if (type) {
-                    return {...{address: siwe.address}, ...type}
-                } else {
-                    return null
-                }
-
-            } catch (e) {
-                console.log("login error",e)
-                return null
-            }
-        },
-    }),
-]
-
-
-export const authOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers,
-    session: {
-        strategy: "jwt",
-    },
-    pages: {
-        signIn: PAGE.Login,
-        signOut: PAGE.Landing,
-    },
-    callbacks: {
-        jwt: async ({token, user}) => {
-            user && (token.user = user)
-            return token
-        },
-        session: async ({session, token}) => {
-            session.user = token.user
-            return session
-        }
-        // jwt: async ({ token, account, profile }) => {
-        //     console.log("jwt", token, account, profile)
-        //     if (account) {
-        //         token.accessToken = account.access_token
-        //         token.id = profile?.id
-        //     }
-        //     return token
-        // },
-        // async session({ session, token }) {
-        //     console.log("session", session)
-        //     console.log("token", token)
-        //     tutaj dodawaja dodatkoe dane
-        //     session.user.address = token.sub
-        //     session.address = token.sub
-        //     return session
-        // },
-    },
-}
 
 export default async function auth(req, res) {
+    const providers = [
+        CredentialsProvider({
+            name: "Ethereum",
+            credentials: {
+                message: {
+                    label: "Message",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+                signature: {
+                    label: "Signature",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+            },
+            async authorize(credentials) {
+                try {
+                    const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"))
+                    const nextAuthUrl = new URL(process.env.NEXTAUTH_URL)
+                    console.log("cookies",req.cookies, meta)
+                    const csrf = req.cookies[meta].split("|")[0]
+                    const result = await siwe.verify({
+                        signature: credentials?.signature || "",
+                        domain: nextAuthUrl.host,
+                        nonce: csrf,
+                    })
+
+                    if (!result.success) return null;
+                    const type = await fetchSessionData(siwe.address)
+
+                    if (type) {
+                        return {...{address: siwe.address}, ...type}
+                    } else {
+                        return null
+                    }
+
+                } catch (e) {
+                    console.log("login error",e)
+                    return null
+                }
+            },
+        }),
+    ]
 
     const isDefaultSigninPage =
         req.method === "GET" && req.query.nextauth.includes("signin")
@@ -96,5 +57,41 @@ export default async function auth(req, res) {
         providers.pop()
     }
 
-    return await NextAuth(req, res, authOptions)
+    return await NextAuth(req, res, {
+        secret: process.env.NEXTAUTH_SECRET,
+        providers,
+        session: {
+            strategy: "jwt",
+        },
+        pages: {
+            signIn: PAGE.Login,
+            signOut: PAGE.Landing,
+        },
+        callbacks: {
+            jwt: async ({token, user}) => {
+                user && (token.user = user)
+                return token
+            },
+            session: async ({session, token}) => {
+                session.user = token.user
+                return session
+            }
+            // jwt: async ({ token, account, profile }) => {
+            //     console.log("jwt", token, account, profile)
+            //     if (account) {
+            //         token.accessToken = account.access_token
+            //         token.id = profile?.id
+            //     }
+            //     return token
+            // },
+            // async session({ session, token }) {
+            //     console.log("session", session)
+            //     console.log("token", token)
+            //     tutaj dodawaja dodatkoe dane
+            //     session.user.address = token.sub
+            //     session.address = token.sub
+            //     return session
+            // },
+        },
+    })
 }
