5c534f6f3affd5a7a1b4d5b78463cb6ac37a0446
hkrawczyk <hello@hkrawczyk.com>
2023-05-24 16:15:00 +0200

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,12 @@ module.exports = {
       output: './out.log',
       error: './error.log',
       log: './combined.outerr.log',
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
     }
   ]
 };
