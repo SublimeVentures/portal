fbd1252d6ef550e3daa637d05a78c05bbbb943ce
hkrawczyk <hello@hkrawczyk.com>
2023-06-03 13:00:46 +0200

    red6

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,3 @@
 export {default} from "next-auth/middleware"
-// import { withAuth } from "next-auth/middleware"
-//
-// // i used advanced middleware configuration
-// export default withAuth(
-//     function middleware(req) {
-//         // some actions here
-//     },
-//     {
-//         callbacks: {
-//             authorized: ({ token }) => {
-//                 // verify token and return a boolean
-//                 return true
-//             },
-//         },
-//     }
-// )
+
 export const config = { matcher: ["/app/:path*"]}
