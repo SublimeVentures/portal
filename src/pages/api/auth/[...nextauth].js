09dd781d92c737005b94a635fd852f2cd5788afb
hkrawczyk <hello@hkrawczyk.com>
2023-06-03 14:29:59 +0200

    fix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -75,9 +75,6 @@ export default async function auth(req, res) {
                 session.user = token.user
                 return session
             },
-            async redirect({url, baseUrl}) {
-              console.log("REDIRECT", url, baseUrl)
-            },
             // async redirect(params: { url: string }) {
             //     const { url } = params
             //
