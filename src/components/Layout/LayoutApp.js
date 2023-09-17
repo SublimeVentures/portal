1b109ce3bf938a8b45b8fb4a311bf1ff7ef3d611
hkrawczyk <hello@hkrawczyk.com>
2023-09-17 16:19:59 +0200

    gtag fix on account

--- src/components/Layout/LayoutApp.js
+++ src/components/Layout/LayoutApp.js
@@ -3,10 +3,10 @@ import {ACLs} from "@/lib/authHelpers";
 import routes from "@/routes";
 import Link from "next/link";
 
-export default function Layout({ children }) {
+export default function LayoutApp({ children }) {
+    const isNT =       children.props.children.props.children[0].props.account.ACL === ACLs.NeoTokyo
+    const notStaked = !children.props.children.props.children[0].props.account.isStaked
 
-    const isNT = children.props.children.props.children.props.account?.ACL === ACLs.NeoTokyo
-    const notStaked = !children.props.children.props.children.props.account?.isStaked
     return (
         <>
             {isNT && notStaked && <div className={"sticky top-0 bg-app-error uppercase text-white font-accent z-[100000] w-full text-center px-5 py-2"}>
@@ -14,7 +14,7 @@ export default function Layout({ children }) {
             </div> }
 
             <div className="flex flex-col collap:flex-row bg-app-bg min-h-screen">
-                <Sidebar account={children.props.children.props.children.props.account}/>
+                <Sidebar account={children.props.children.props.children[0].props.account}/>
                 <main className="flex flex-col w-full grow sm:min-h-screen max-w-[1920px] p-5 mobile:p-10 gap-5 mobile:gap-10 text-app-white">{children}</main>
                 {/*<main className="flex flex-col w-full sm:min-h-screen max-w-[1920px] p-5 mobile:p-10 gap-5 mobile:gap-10 text-app-white">{children}</main>*/}
             </div>
