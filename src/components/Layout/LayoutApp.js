9b59c5204aff10810f8a11aacf7a69519ae0e445
hkrawczyk <hello@hkrawczyk.com>
2023-09-25 16:47:56 +0200

    auth fix3

--- src/components/Layout/LayoutApp.js
+++ src/components/Layout/LayoutApp.js
@@ -14,7 +14,7 @@ export default function LayoutApp({ children }) {
             </div> }
 
             <div className="flex flex-col collap:flex-row bg-app-bg min-h-screen">
-                <Sidebar account={children.props.children.props.children[0].props.account}/>
+                <Sidebar account={children.props.children.props.children.props?.account}/>
                 <main className="flex flex-col w-full grow sm:min-h-screen max-w-[1920px] p-5 mobile:p-10 gap-5 mobile:gap-10 text-app-white">{children}</main>
                 {/*<main className="flex flex-col w-full sm:min-h-screen max-w-[1920px] p-5 mobile:p-10 gap-5 mobile:gap-10 text-app-white">{children}</main>*/}
             </div>
