8096e0cf0b7305a5525f763a459390fb744325d9
hkrawczyk <hello@hkrawczyk.com>
2023-11-18 14:33:16 +0100

    store

--- server/controllers/upgrade.js
+++ server/controllers/upgrade.js
@@ -1,48 +1,143 @@
 const {ACLs} = require("../../src/lib/authHelpers");
-const {processUseUpgrade, fetchUpgrade} = require("../queries/upgrade.query");
+const {
+    fetchUpgrade, fetchAppliedUpgradesInTransaction, saveUpgradeUse
+} = require("../queries/upgrade.query");
 const {PremiumItemsENUM} = require("../../src/lib/enum/store");
+const logger = require("../services/logger");
+const {serializeError} = require("serialize-error");
+const {isBased} = require("@/lib/utils");
+const {PremiumItemsParamENUM} = require("@/lib/enum/store");
+const db = require("../services/db/db.init");
+const {getStoreItemsOwnedByUser, updateUserUpgradeAmount} = require("../queries/storeUser.query");
+const {UPGRADE_ERRORS} = require("../enum/UpgradeErrors");
+const {getOfferById} = require("../queries/offers.query");
+const {bookAllocationGuaranteed} = require("../queries/invest.query");
 
-async function useUpgrade(user, req) {
-    let offerId, upgradeId;
+async function useGuaranteed(offerId, user, transaction) {
+    const {ACL, multi, userId, allocationBonus} = user
 
-    try {
-        offerId = Number(req.params.id)
-        upgradeId = Number(req.params.upgrade)
+    const userAppliedUpgrades = await fetchAppliedUpgradesInTransaction(userId, offerId, transaction)
+    if (!userAppliedUpgrades.ok) {
+        return userAppliedUpgrades
+    }
 
-        if(upgradeId !== PremiumItemsENUM.Guaranteed && upgradeId !== PremiumItemsENUM.Increased) {
-            throw new Error("Wrong Upgrade")
-        }
+    const upgradeGuaranteed = userAppliedUpgrades.find(el => el.storeId === PremiumItemsENUM.Guaranteed)?.amount
+    const upgradeIncreased = userAppliedUpgrades.find(el => el.storeId === PremiumItemsENUM.Increased)?.amount
 
-    } catch(e) {
-        console.log("erro",e)
+    if (upgradeGuaranteed) {
+        await transaction.rollback();
         return {
-            ok:false
+            ok: false,
+            error: UPGRADE_ERRORS.GuaranteedUsed
         }
     }
 
-    const {ACL, address ,id, multi} = user
-    const owner = ACL === ACLs.Whale ? id : address
-    const data = await processUseUpgrade(owner, offerId, upgradeId, id, ACL, multi)
-    console.log("Data",data)
+    const offer = await getOfferById(offerId)
+    const increasedAllocations = upgradeIncreased * PremiumItemsParamENUM.Increased
+    const isSeparatePool = offer.alloTotalPartner > 0 && upgradeIncreased !== ACLs.Whale;
+    const totalAllocation = offer[isSeparatePool ? 'alloTotalPartner' : 'alloTotal']
 
-    return data
+    let amountMax
+    if (isBased) {
+        amountMax = multi * offer.alloMin + (increasedAllocations ? increasedAllocations : 0)
+    } else {
+        amountMax = (totalAllocation * multi) + allocationBonus + (increasedAllocations ? increasedAllocations : 0)
+    }
+
+    const amount = ACL === ACLs.Whale ? PremiumItemsParamENUM.Guaranteed : ((amountMax < PremiumItemsParamENUM.Guaranteed ? amountMax : PremiumItemsParamENUM.Guaranteed) * (100 - offer.tax) / 100)
+
+    const increaseGuaranteedReservations = await bookAllocationGuaranteed(offerId, amount, totalAllocation, isSeparatePool, transaction)
+    if (!increaseGuaranteedReservations.ok) {
+        return increaseGuaranteedReservations
+    }
+
+    return {
+        ok: true,
+        data: {amount}
+    }
 }
 
-async function getUpgrades(user, req) {
-    let offerId
+async function useUpgrade(user, req) {
+    let offerId, storeId, transaction;
+
     try {
         offerId = Number(req.params.id)
+        storeId = Number(req.params.upgrade)
 
-    } catch(e) {
-        console.log("erro",e)
+        if (storeId !== PremiumItemsENUM.Guaranteed && storeId !== PremiumItemsENUM.Increased) {
+            throw new Error("Wrong Upgrade")
+        }
+
+        const {userId} = user
+
+        transaction = await db.transaction();
+
+        //check if user has upgrades
+        const isUserOwnsUpgrade = await getStoreItemsOwnedByUser(userId, storeId, transaction)
+        if (!isUserOwnsUpgrade.ok) {
+            return isUserOwnsUpgrade
+        }
+
+        let allocation
+        //check if user used Guaranteed (limit - only one)
+        if (storeId === PremiumItemsENUM.Guaranteed) {
+            const saveStatus_upgrade = await useGuaranteed(offerId, user, transaction)
+            if (!saveStatus_upgrade.ok) {
+                logger.warn('ERROR :: [useUpgrade] - guaranteed couldnt apply', {
+                    saveStatus_upgrade,
+                    isUserOwnsUpgrade,
+                    user,
+                    prams: req.params
+                });
+                return saveStatus_upgrade
+            }
+            allocation = saveStatus_upgrade.data.amount
+        }
+
+        //save used upgrade
+        const save = await saveUpgradeUse(userId, offerId, storeId, 1, allocation, transaction)
+        if (!save.ok) {
+            logger.warn('ERROR :: [useUpgrade] - couldnt save use of upgrade', {save, user, offerId, storeId});
+            return save
+        }
+
+        const deduct = await updateUserUpgradeAmount(userId, storeId, -1, transaction)
+        if (!deduct.ok) {
+            return deduct
+        }
+
+        await transaction.commit();
         return {
-            ok:false,
+            ok: true
+        }
+
+    } catch (error) {
+        if (transaction) {
+            await transaction.rollback();
+        }
+        logger.error('QUERY :: [useUpgrade]', {error: serializeError(error), offerId, user, params: req.params});
+        return {
+            ok: false,
+            error: UPGRADE_ERRORS.Unexpected
+        }
+    }
+}
+
+
+async function getUpgrades(user, req) {
+    try {
+        const offerId = Number(req.params.id)
+        const {userId} = user
+        return {ok: true, data: await fetchUpgrade(userId, offerId)}
+
+    } catch (error) {
+        logger.error('QUERY :: [getUpgrades]', {error: serializeError(error), params: req.params, user});
+        return {
+            ok: false,
             usedUpgrades: {}
         }
     }
 
-    const { userId} = user
-    return {ok: true, data: await fetchUpgrade(userId, offerId)}
 
 }
 
