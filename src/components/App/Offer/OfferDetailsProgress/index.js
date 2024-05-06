b07eece385633bac7c7a9c64fdeec627e3bdfbaa
marek3289 <marek2494@o2.pl>
2024-05-06 13:23:08 +0200

    fix(fomobar): fixed overflow

--- src/components/App/Offer/OfferDetailsProgress/index.js
+++ src/components/App/Offer/OfferDetailsProgress/index.js
@@ -13,11 +13,11 @@ const defaultProgressColors = {
 export default function OfferDetailsProgress({ allocations, isSoldOut, progressColors = defaultProgressColors }) {
     const tilt = useRef(null);
     const { base_percentage, base_rounding, res_percentage, res_rounding, guaranteed_percentage, guaranteed_rounding } = useOfferDetailsProgress(allocations, isSoldOut)
-    
+
     useEffect(() => VanillaTilt.init(tilt.current, { scale: 1.05, speed: 1000, max: 5 }), []);
 
     return (
-        <div className={cn('relative h-[50px] w-full flex flex-row items-center rounded-xl select-none', { 'overflow-hidden': base_percentage >= 95 })} ref={tilt}>
+        <div className={cn('relative h-[50px] w-full flex flex-row items-center rounded-xl select-none pointer-events-none overflow-hidden hover:overflow-visible')} ref={tilt}>
             <div className="os-progress-bar absolute rounded-xl overflow-hidden -z-10">
                 <span className="os-progress-bar--meter flex flex-1 rounded-tl-xl rounded-bl-xl"></span>
                 <span
@@ -30,7 +30,7 @@ export default function OfferDetailsProgress({ allocations, isSoldOut, progressC
                 text={null}
                 wrapper={
                     <div
-                        className="w-full h-full z-10 opacity-10 bg-[var(--progress-step-color)] rounded-tl-xl rounded-bl-xl cursor-pointer transition-all duration-150 hover:opacity-100 hover:border-2 hover:z-40 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
+                        className="pointer-events-auto w-full h-full z-10 opacity-10 bg-[var(--progress-step-color)] rounded-tl-xl rounded-bl-xl cursor-pointer transition-all duration-150 hover:opacity-100 hover:border-2 hover:z-40 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
                         style={{
                             borderTopRightRadius: base_rounding,
                             borderBottomRightRadius: base_rounding,
@@ -45,7 +45,7 @@ export default function OfferDetailsProgress({ allocations, isSoldOut, progressC
                 text={`Booked ${res_percentage}%`}
                 wrapper={
                     <div
-                        className="w-full h-full z-20 opacity-10 bg-[var(--progress-step-color)] cursor-pointer transition-all duration-150 hover:z-40 hover:opacity-100 hover:border-2 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
+                        className="pointer-events-auto w-full h-full z-20 opacity-10 bg-[var(--progress-step-color)] cursor-pointer transition-all duration-150 hover:z-40 hover:opacity-100 hover:border-2 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
                         style={{
                             borderTopLeftRadius: base_percentage === 0 ? '12px' : '0',
                             borderBottomLeftRadius: base_percentage === 0 ? '12px' : '0',
@@ -61,10 +61,10 @@ export default function OfferDetailsProgress({ allocations, isSoldOut, progressC
                 text={`Guaranteed ${guaranteed_percentage}%`}
                 wrapper={
                     <div
-                        className="w-full h-full z-20 opacity-10 bg-[var(--progress-step-color)] cursor-pointer transition-all duration-150 hover:z-40 hover:opacity-100 hover:border-2 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
+                        className="pointer-events-auto w-full h-full z-20 opacity-10 bg-[var(--progress-step-color)] cursor-pointer transition-all duration-150 hover:z-40 hover:opacity-100 hover:border-2 hover:border-[var(--progress-step-color)] hover:shadow-[0_0_2px_var(--progress-step-color),inset_0_0_2px_var(--progress-step-color),0_0_0px_var(--progress-step-color),0_0_0_var(--progress-step-color),0_0_10px_var(--progress-step-color)]"
                         style={{
-                            borderTopLeftRadius: base_percentage + res_percentage === 0 ? '12px' : '0',
-                            borderBottomLeftRadius: base_percentage + res_percentage === 0 ? '12px' : '0',
+                            borderTopLeftRadius: base_percentage + res_percentage <= 1 ? '12px' : '0',
+                            borderBottomLeftRadius: base_percentage + res_percentage <= 1 ? '12px' : '0',
                             borderTopRightRadius: guaranteed_rounding,
                             borderBottomRightRadius: guaranteed_rounding,
                         }}
