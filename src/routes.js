3fefdaf8aff0c416f263dfea64f8ee2b8e4c657d
hkrawczyk <hello@hkrawczyk.com>
2024-06-28 18:31:41 +0200

    new wiki routes

--- src/routes.js
+++ src/routes.js
@@ -40,22 +40,22 @@ export const API = {
 };
 
 const ExternalLinksBased = {
-    BOOKING_SYSTEM: "https://info.basedvc.fund/Allocation-Booking-System-2f93893f882c49d0ab305159aa7099c4",
-    OFFER_PHASES: "https://info.basedvc.fund/Offer-phases-cf284a30c16f4586a8f2fa6b49df1e8d",
-    AFTER_INVESTMENT: "https://info.basedvc.fund/After-Investing-7a44086b917545029d95574c53c66a7d",
-    SUPPORTED_NETWORKS: "https://info.basedvc.fund/Supported-networks-safe-gas-3d50096070d54efebae0590d02acbcdb",
-    UPGRADES: "https://info.basedvc.fund/Upgrades-adb5bd92e38644ef95433d23be612bf1", //todo: fix images
-    LOOTBOX: "https://info.basedvc.fund/MysteryBox-0b34290a907543049b2cef882ba72e5c", //todo: fix
-    HOW_TO_ACCESS: "https://info.basedvc.fund/How-to-access-3VC-f40a1142d93f4b0ba38e13114189a877",
-    DELEGATED_ACCESS: "https://info.basedvc.fund/Delegated-access-dc60abd8a5654641a7bd77d537256aa7",
-    VAULT: "https://info.basedvc.fund/App-Navigation-2f8097958f69467aaa03bbfda0ecd89e",
+    BOOKING_SYSTEM:
+        "https://info.basedvc.fund/overview/faq#how-does-the-booking-system-work-for-investments-on-the-basedvc-platform",
+    OFFER_PHASES: "https://info.basedvc.fund/overview/the-investment-process#the-investment-phases-are-as-follows",
+    AFTER_INVESTMENT: "https://info.basedvc.fund/overview/managing-your-investments",
+    SUPPORTED_NETWORKS: "https://info.basedvc.fund/overview/multichain",
+    UPGRADES: "https://info.basedvc.fund/overview/upgrades",
+    LOOTBOX: "https://info.basedvc.fund/overview/mystery-boxes",
+    HOW_TO_ACCESS: "https://info.basedvc.fund/overview/how-to-join",
+    DELEGATED_ACCESS: "https://info.basedvc.fund/overview/faq#how-does-wallet-delegation-work",
+    VAULT: "https://info.basedvc.fund/overview/managing-your-investments",
     WIKI: "https://info.basedvc.fund",
     DISCORD: "https://discord.gg/AsGvs6uRU8",
     TWITTER: "https://twitter.com/basedvcfund",
-    OTC: "https://info.basedvc.fund/OTC-fdad47130a0a49bbaa1a1e4c9fd95907",
+    OTC: "https://info.basedvc.fund/overview/otc-sales",
     APPLY: "https://tally.so/r/mK5Pvk",
     WHALE_CLUB: "https://info.basedvc.fund/3VC-Whale-Club-5fea374623c1493d8af2b4b04914ab3e",
-    //todo: https://www.notion.so/basedvc/Investment-Cap-Model-5b685123fe3c43e9a29e6d8fa67be2a3 - to be fixed
 };
 
 const ExternalLinksCitCap = {
