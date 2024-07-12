290514044f7d8bc85d19f02ca26464a2f2de3494
marek3289 <marek2494@o2.pl>
2024-07-12 11:45:26 +0200

    fix(notifications): fix after merge

--- src/v2/components/Notification/NotificationMenu.js
+++ src/v2/components/Notification/NotificationMenu.js
@@ -20,21 +20,15 @@ const NotificationMenu = ({ isBlockedAlert }) => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <IconButton icon={bellIcon} variant="transparent" shape="circle" className="outline-none" />
+                <IconButton icon={bellIcon} variant="transparent" shape="circle" className="p-3 outline-none" />
             </DropdownMenuTrigger>
-
-  return (
-    <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <IconButton icon={bellIcon} variant="transparent" shape="circle" className="p-3 outline-none" />
-        </DropdownMenuTrigger>
-        
-        <DropdownMenuContent
-            style={{ ...layoutStyles, "--alertHeight": isBlockedAlert ? layoutStyles["--alertHeight"] : "0px" }}
-            className="rounded-b-lg w-screen h-[calc(100vh_-_var(--navbarHeight)_-_var(--headerHeight)_-_var(--alertHeight))] overflow-auto sm:max-w-[315px] sm:h-auto sm:rounded-b"
-        >
-            <div className="px-8 flex justify-between sm:flex-col">
-                <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+            
+            <DropdownMenuContent
+                style={{ ...layoutStyles, "--alertHeight": isBlockedAlert ? layoutStyles["--alertHeight"] : "0px" }}
+                className="rounded-b-lg w-screen h-[calc(100vh_-_var(--navbarHeight)_-_var(--headerHeight)_-_var(--alertHeight))] overflow-auto sm:max-w-[315px] sm:h-auto sm:rounded-b"
+            >
+                <div className="px-8 flex justify-between sm:flex-col">
+                    <DropdownMenuLabel>Notifications</DropdownMenuLabel>
                     <div className="flex items-center">
                         <h3 className="mr-2 text-lg font-medium text-foreground">
                             New <span className="font-light">({newCount})</span>
