cd4cd08be63d63baa5bf089b9409baf5b5f919ab
Michał Kamil Przybysz <m.przybysz@blackmoose.pl>
2024-07-22 16:50:05 +0200

    CU-86b13nh7q - New Design - Create Upgrade View

--- src/pages/app/upgrades.js
+++ src/pages/app/upgrades.js
@@ -3,11 +3,12 @@ import Head from "next/head";
 import dynamic from "next/dynamic";
 import { useQuery } from "@tanstack/react-query";
 import { AiOutlineRead as ReadIcon } from "react-icons/ai";
+import Image from "next/image";
 import { ButtonTypes, UniButton } from "@/components/Button/UniButton";
 import routes, { ExternalLinks } from "@/routes";
 import { ButtonIconSize } from "@/components/Button/RoundButton";
 import Empty from "@/components/App/Empty";
-import LayoutApp from "@/components/Layout/LayoutApp";
+import { AppLayout, Metadata } from "@/v2/components/Layout";
 import Loader from "@/components/App/Loader";
 import { PremiumItemsENUM } from "@/lib/enum/store";
 import StoreItem from "@/components/App/Store/StoreItem";
@@ -15,6 +16,10 @@ import { fetchStore } from "@/fetchers/store.fetcher";
 import { getCopy } from "@/lib/seoConfig";
 import { processServerSideData } from "@/lib/serverSideHelpers";
 import { useEnvironmentContext } from "@/lib/context/EnvironmentContext";
+import { Card } from "@/v2/components/ui/card";
+import { cn } from "@/lib/cn";
+import { getCurrency } from "@/components/App/Store/helper";
+import { Button } from "@/v2/components/ui/button";
 
 const BuyStoreItemModal = dynamic(() => import("@/components/App/Store/BuyStoreItemModal"), { ssr: false });
 
@@ -41,20 +46,6 @@ export default function AppUpgrades({ session }) {
     const storeData = response?.filter((el) => el.id !== PremiumItemsENUM.MysteryBox);
     const currency = getCurrencyStore()[0];
 
-    const renderPage = () => {
-        if (isLoading) return <Loader />;
-        if (!storeData || storeData.length === 0) return <Empty />;
-
-        return (
-            <div className="grid grid-cols-12 gap-y-8 mobile:gap-10">
-                {!!storeData &&
-                    storeData.map((el) => (
-                        <StoreItem item={el} key={el.id} cdn={cdn} setOrder={setOrder} currency={currency} />
-                    ))}
-            </div>
-        );
-    };
-
     useEffect(() => {
         if (order) {
             setBuyModal(true);
@@ -78,28 +69,49 @@ export default function AppUpgrades({ session }) {
             <Head>
                 <title>{title}</title>
             </Head>
-            <div className="flex flex-col justify-between gap-7 sm:flex-row">
-                <div className="flex flex-col justify-center">
-                    <div className="glow text-3xl page-header-text">UPGRADES</div>
-                    <div className="text-outline text-md mt-2 white min-w-[250px]">Supercharge your investments.</div>
-                </div>
-                <div className="mx-auto flex items-center sm:ml-auto sm:mr-0">
-                    <div>
-                        <UniButton
-                            type={ButtonTypes.BASE}
-                            text="Learn more"
-                            isWide={true}
-                            size="text-sm sm"
-                            handler={() => {
-                                window.open(ExternalLinks.UPGRADES, "_blank");
-                            }}
-                            icon={<ReadIcon className={ButtonIconSize.hero} />}
-                        />
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-1 flex-col select-none items-center gap-y-5 mobile:gap-y-10 mobile:gap-10 page-content-text">
-                {renderPage()}
+            <div className="flex grow 3xl:px-19 3xl:py-12 3xl:gap-11">
+                {!!storeData &&
+                    storeData.map((data, index) => (
+                        <Card
+                            key={data.slug}
+                            variant={data.id === 1 ? "accent" : "default"}
+                            className="text-white flex-1 flex flex-col gap-11 py-14 items-center justify-center"
+                        >
+                            <Image
+                                src={`${cdn}/webapp/store/${data.img}`}
+                                className="rounded-full size-80"
+                                alt={data.name}
+                                width={320}
+                                height={320}
+                            />
+                            <div className="w-4/5 mx-auto">
+                                <h1
+                                    className={cn("text-center text-9xl mb-6", {
+                                        "text-accent": !index,
+                                        "text-primary": !!index,
+                                    })}
+                                >
+                                    {data.name}
+                                </h1>
+                                <p className="text-center text-lg">{data.description}</p>
+                            </div>
+                            <div className="flex items-center px-11 py-6 rounded-md bg-white/5 backdrop-blur-2xl w-9/12 mx-auto">
+                                <dl className="grid grid-rows-2 grid-flow-col w-2/3">
+                                    <DefinitionTerm>Type</DefinitionTerm>
+                                    <DefinitionDescription>
+                                        {data.id === 1 ? "Not Stackable" : "Stackable"}
+                                    </DefinitionDescription>
+                                    <DefinitionTerm>Price</DefinitionTerm>
+                                    <DefinitionDescription>
+                                        {data.price} {currency && getCurrency(currency.symbol)}
+                                    </DefinitionDescription>
+                                </dl>
+                                <Button className="w-1/3" variant={data.id === 1 ? "accent" : "default"}>
+                                    Buy
+                                </Button>
+                            </div>
+                        </Card>
+                    ))}
             </div>
             <BuyStoreItemModal
                 model={isBuyModal}
@@ -112,10 +124,18 @@ export default function AppUpgrades({ session }) {
     );
 }
 
+function DefinitionTerm({ children }) {
+    return <dt className="text-md font-light leading-5 self-end">{children}</dt>;
+}
+
+function DefinitionDescription({ children }) {
+    return <dd className="text-2xl leading-7">{children}</dd>;
+}
+
 export const getServerSideProps = async ({ req, res }) => {
     return await processServerSideData(req, res, routes.Upgrades);
 };
 
 AppUpgrades.getLayout = function (page) {
-    return <LayoutApp>{page}</LayoutApp>;
+    return <AppLayout>{page}</AppLayout>;
 };
