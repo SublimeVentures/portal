b3c80b4fb9fe59134d27d04541015eab2b6b5b87
Miłosz Kramek <milosz.kramek@verto.org.pl>
2024-07-29 16:38:28 +0200

    Review changes - add message to error

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -3,7 +3,10 @@ const { verifyID } = require("../../src/lib/authHelpers");
 module.exports = async function authMiddleware(req, res, next) {
     const { auth, user } = await verifyID(req);
     if (!auth) {
-        return res.status(401).json({ ok: false });
+        return res.status(401).json({
+            ok: false,
+            error: "Not authorized",
+        });
     } else {
         req.user = user;
         next();
