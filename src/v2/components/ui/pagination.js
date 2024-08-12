999a1b98cb78e11a63588864612beeb391f8d599
marek3289 <marek2494@o2.pl>
2024-08-12 13:11:08 +0200

    feat(pagination): created pagination ui component

---
+++ src/v2/components/ui/pagination.js
@@ -0,0 +1,86 @@
+import { forwardRef } from "react";
+import Link from "next/link";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+ 
+import { cn } from "@/lib/cn";
+ 
+const Pagination = ({ className, ...props }) => (
+    <nav
+        role="navigation"
+        aria-label="pagination"
+        className={cn("mx-auto flex w-full justify-center", className)}
+        {...props}
+    />
+)
+
+Pagination.displayName = "Pagination"
+ 
+const PaginationContent = forwardRef(({ className, ...props }, ref) => (
+    <ul
+        ref={ref}
+        className={cn("relative flex flex-row items-center gap-4 bg-[#082131] rounded", className)}
+        {...props}
+    />
+));
+
+PaginationContent.displayName = "PaginationContent";
+ 
+const PaginationItem = forwardRef(({ className, ...props }, ref) => <li ref={ref} className={className} {...props} />);
+
+PaginationItem.displayName = "PaginationItem";
+
+const PaginationLink = ({ className, isActive, size = "icon", ...props }) => (
+    <Link
+        aria-current={isActive ? "page" : undefined}
+        className={cn(
+            "p-3 h-10 w-10 flex items-center justify-center text-sm text-foreground rounded cursor-pointer",
+            isActive ? "bg-accent shadow-accent" : "bg-[#082131] hover:bg-primary-700",
+        )}
+        {...props}
+    />
+);
+
+PaginationLink.displayName = "PaginationLink";
+ 
+const PaginationPrevious = ({ className, ...props }) => (
+    <Link
+        aria-label="Go to previous page"
+        className={cn("absolute -left-13 p-3 h-10 w-10 flex items-center justify-center text-foreground bg-[#082131] rounded hover:bg-primary-700", className)}
+        {...props}
+    >
+        <ChevronLeftIcon />
+    </Link>
+);
+
+PaginationPrevious.displayName = "PaginationPrevious";
+ 
+const PaginationNext = ({ className, ...props }) => (
+    <Link
+        aria-label="Go to next page"
+        className={cn("absolute -right-13 p-3 h-10 w-10 flex items-center justify-center text-foreground bg-[#082131] rounded hover:bg-primary-700", className)}
+        {...props}
+    >
+        <ChevronRightIcon />
+    </Link>
+);
+
+PaginationNext.displayName = "PaginationNext";
+ 
+const PaginationEllipsis = ({ className, ...props }) => (
+    <span aria-hidden className={cn("p-3 h-10 w-10 flex items-center justify-center text-sm text-foreground pointer-events-none", className)} {...props}>
+        <span className="sr-only">More pages</span>
+        <span>...</span>
+    </span>
+);
+
+PaginationEllipsis.displayName = "PaginationEllipsis";
+ 
+export {
+    Pagination,
+    PaginationContent,
+    PaginationLink,
+    PaginationItem,
+    PaginationPrevious,
+    PaginationNext,
+    PaginationEllipsis,
+};
