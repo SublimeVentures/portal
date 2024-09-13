fea637913b39c53f6cd265280334d279bae3d586
marek3289 <marek2494@o2.pl>
2024-09-13 12:05:00 +0200

    feature(offer-details): update invest block title

--- src/v2/modules/offer/Invest/index.js
+++ src/v2/modules/offer/Invest/index.js
@@ -1,6 +1,7 @@
 import { useOfferDetailsStore } from "@/v2/modules/offer/store";
 import { PhaseId } from "@/v2/lib/phases";
 import usePhaseInvestment from "@/v2/hooks/usePhaseInvestment";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/v2/components/ui/tooltip";
 import { Badge } from "@/v2/components/ui/badge";
 import { cn } from "@/lib/cn";
 
@@ -14,7 +15,7 @@ import UpgradesModal from "./Modals/UpgradesModal";
 // const data = useInvestContext(offer.id);
 let isClosed = false; // @TODO: Remove
 
-export default function Investment({ session }) {
+export default function Investment({ session, className }) {
     const { phaseCurrent, isClosed: isClosedTemp } = usePhaseInvestment();
     const { upgradesUse } = useOfferDetailsStore();
     const { data: offer } = useOfferDetailsQuery();
@@ -28,7 +29,18 @@ export default function Investment({ session }) {
     return (
         <div className={cn("p-4 flex flex-col space-y-4 rounded bg-[#12202C]", className)}>
             <div className="flex flex-wrap items-center justify-between gap-4 xl:flex-nowrap">
-                <h2 className="text-lg font-semibold 2xl:mr-4">Invest</h2>
+                <div className="flex gap-2 items-center">
+                    <h2 className="text-lg font-semibold">Invest</h2>
+                    <Tooltip>
+                        <TooltipTrigger
+                            className="size-4 rounded-full bg-white/[.14] text-2xs flex items-center justify-center cursor-help hover:bg-white/[.2] transition-colors duration-200"
+                            asChild
+                        >
+                            <span>?</span>
+                        </TooltipTrigger>
+                        <TooltipContent>Something to show</TooltipContent>
+                    </Tooltip>
+                </div>
 
                 <div className="ml-auto flex items-center gap-4 xl:order-3 xl:ml-0">
                     <UpgradesModal />
