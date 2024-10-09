4da7c7fd15a1aaf76cb131f154f499fd67764205
marek3289 <marek2494@o2.pl>
2024-10-09 15:19:38 +0200

    fixes

--- src/v2/modules/offer/Invest/Modals/CalculateModal/CalculateForm.js
+++ src/v2/modules/offer/Invest/Modals/CalculateModal/CalculateForm.js
@@ -1,12 +1,28 @@
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
+import useCalculate from "./useCalculate";
 import { Input } from "@/v2/components/ui/input";
 import { IconButton } from "@/v2/components/ui/icon-button";
-import useCalculate from "./useCalculate";
+
+// @TODO - Data taken from useInvest. Find better way to store it (tenant config?)
+const taxPercentage = 10;
+
+// @todo - create universal definition - taken from fundraise
+const Definition = ({ term, isLoading, children }) => (
+    <>
+        <dt>{term}:</dt>
+        <dd className="text-right justify-self-end font-medium">{children}</dd>
+    </>
+);
 
 export default function CalculateForm() {
-    const { state: { amount, price, multiplier }, handleAmountChange, handleMultiplierChange } = useCalculate();
+    const {
+        state: { amount, price, multiplier },
+        handleAmountChange,
+        handleMultiplierChange,
+    } = useCalculate();
     const multiplierParsed = multiplier.toFixed(2);
+    const subtotal = price - price * (taxPercentage / 100);
 
     return (
         <div className="flex flex-col gap-4">
@@ -14,7 +30,9 @@ export default function CalculateForm() {
                 <span className="text-base mb-2 lg:text-lg">Buying Allocation</span>
                 <div className="relative w-full ">
                     <Input type="fund" value={amount} onChange={handleAmountChange} className="px-4 w-full md:px-8" />
-                    <span className="px-4 absolute right-0 top-1/2 -translate-y-1/2 text-foreground/50 font-light md:px-8">USD</span>
+                    <span className="px-4 absolute right-0 top-1/2 -translate-y-1/2 text-foreground/50 font-light md:px-8">
+                        USD
+                    </span>
                 </div>
             </label>
 
@@ -25,12 +43,19 @@ export default function CalculateForm() {
             </div>
 
             <label className="w-full flex flex-col text-foreground">
-                <span className="text-base mb-2 lg:text-lg">Return</span>
+                <span className="text-base mb-2 lg:text-lg">Return total</span>
                 <div className="relative w-full ">
                     <Input value={price} className="px-4 w-full md:px-8" />
-                    <span className="px-4 absolute right-0 top-1/2 -translate-y-1/2 text-foreground/50 font-light md:px-8">USD</span>
+                    <span className="px-4 absolute right-0 top-1/2 -translate-y-1/2 text-foreground/50 font-light md:px-8">
+                        USD
+                    </span>
                 </div>
             </label>
+
+            <dl className="grid grid-cols-2 gap-2 text-sm font-light text-foreground/50 select-none">
+                <Definition term="Tax fees">{taxPercentage}%</Definition>
+                <Definition term="Subtotal">${subtotal}</Definition>
+            </dl>
         </div>
     );
-};
+}
