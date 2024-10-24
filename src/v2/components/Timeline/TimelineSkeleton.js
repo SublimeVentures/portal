5698e818a95dfc9911f036fed46b0e3112b3140a
Michał Kamil Przybysz <m.przybysz@blackmoose.pl>
2024-10-24 13:22:37 +0200

    fix: notification loading (#486)

--- src/v2/components/Timeline/TimelineSkeleton.js
+++ src/v2/components/Timeline/TimelineSkeleton.js
@@ -1,9 +1,9 @@
 import { Skeleton } from "@/v2/components/ui/skeleton";
 import TimelineSVG from "@/v2/assets/svg/timeline.svg";
 
-export default function TimelineSkeleton({ count = 6, showTimeline = true }) {
-    return Array.from({ length: count }).map((_, index) => (
-        <div key={index} className="flex text-sm group">
+export default function TimelineSkeleton({ showTimeline = true }) {
+    return (
+        <div className="flex text-sm">
             {showTimeline ? (
                 <div className="mx-4 flex flex-col justify-between items-center gap-2 before:w-0 before:border-white/20 before:border before:h-full after:w-0 after:border after:border-white/20 after:h-full group-first:before:border-none group-last:after:border-none">
                     <div>
@@ -12,7 +12,7 @@ export default function TimelineSkeleton({ count = 6, showTimeline = true }) {
                 </div>
             ) : null}
 
-            <Skeleton className="my-2 h-23 shrink-0" />
+            <Skeleton className="my-2 h-23" />
         </div>
-    ));
+    );
 }
